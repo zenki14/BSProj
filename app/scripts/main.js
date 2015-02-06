2dd399fb398e3010b3c3ffbc1c080dc4d12412a1
Dane Worsham <zenki14@gmail.com>
2015-02-06 09:58:44 -0500

    initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,49 @@
-console.log('The Iron Yard Rocks');
+var Player1 = function(options) {
+	var args = options || {};
+
+}
+
+//Player Instance
+
+
+var Computer = function(options) {
+	var args = options || {};
+
+}
+
+//Computer Instance
+
+var Ships = function(options) {
+	var args = options || {};
+
+	this.type = args.type;
+	this.size = args.size;
+	this.position = args.position;
+	this.hit = false;
+	// this.miss = args.miss;
+}
+
+Ships.prototype.hit = function (shipHit, hitUpdate) {
+	shipHit.hit = hitUpdate;
+	if (hitUpdate === true) {
+		this.hit === true
+	}
+}
+
+Ships.prototype.position = function (x, y, direction) {
+
+
+	Ships.position = 
+}
+
+
+//Ships Instances
+
+var BS = new Ships ({type: 'Battleship', size: 4});
+var AC = new Ships ({type: 'Aircraft Carrier', size: 5});
+var D = new Ships ({type: 'Destroyer', size: 3});
+var PB = new Ships ({type : 'Patrol Boat', size: 2});
+var S = new Ships ({type: 'Submarine', type: 3}) 
+
+
+
